dc7f8663d568c8359f0b78a78ef0d1fae3f75a6d
Kapa <kapa@sipaksi.net>
2014-06-02 17:05:19 +0200

    Added error checking to parseToDom. XML is a pain in the arse in the browser...

--- xml.js
+++ xml.js
@@ -34,13 +34,25 @@ define(function () {
 		 * @returns {Document}
 		 */
 		parseToDom: function (s) {
+			var res, parser;
+			var parseError = new Error('XML is malformed and cannot be parsed.');
 			if (typeof window.DOMParser != "undefined") {
-				return (new window.DOMParser()).parseFromString(s, "text/xml");
+				parser = new window.DOMParser();
+				res = parser.parseFromString(s, "text/xml");
+				var errorNs = parser.parseFromString('<', 'text/xml')
+					.getElementsByTagName('parsererror')[0].namespaceURI;
+				if (res.getElementsByTagNameNS(errorNs, 'parsererror').length) {
+					throw parseError;
+				}
+				return res;
 			} else if (typeof window.ActiveXObject != "undefined" && new window.ActiveXObject("Microsoft.XMLDOM")) {
-				var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
-				xmlDoc.async = "false";
-				xmlDoc.loadXML(s);
-				return xmlDoc;
+				parser = new window.ActiveXObject("Microsoft.XMLDOM");
+				parser.async = "false";
+				res = parser.loadXML(s);
+				if (!res) {
+					throw parseError;
+				}
+				return parser;
 			} else {
 				throw new Error("No XML parser found.");
 			}
